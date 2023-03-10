521542f0c4f39376c532df4ccfbcb18cc14f99cd
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-03-10 15:12:29 +0200

    added cycle for second homework

--- hw02/script.js
+++ hw02/script.js
@@ -1,26 +1,29 @@
+let sum = 0;
 let first_number = prompt('Введіть перше число: ');
 let second_number = prompt('Введіть друге число: ');
 let cond = confirm("Пропустити парні числа?");
-let sum = 0;
 
-if (isNaN(first_number) || isNaN(second_number)){
+while (isNaN(first_number) || isNaN(second_number) || parseInt(second_number) < parseInt(first_number) || parseInt(first_number) <= 0){
     console.log("Неправильний ввід");
-} else {
-    first_number = parseInt(first_number);
-    second_number = parseInt(second_number);
-    if (first_number > second_number){
-        let temp = first_number;
-        first_number = second_number;
-        second_number = temp;
-    }
-    for (let i = first_number; i <= second_number; i++){
-        if (cond) { 
-            if (i % 2 != 0){
-                sum += i;
-            }
-        } else {
+    first_number = prompt('Введіть перше число: ');
+    second_number = prompt('Введіть друге число: ');
+    cond = confirm("Пропустити парні числа?");
+}  
+ 
+first_number = parseInt(first_number);
+second_number = parseInt(second_number);
+if (first_number > second_number){
+    let temp = first_number;
+    first_number = second_number;
+    second_number = temp;
+}
+for (let i = first_number; i <= second_number; i++){
+    if (cond) { 
+        if (i % 2 != 0){
             sum += i;
         }
+    } else {
+        sum += i;
     }
-    console.log(sum)
 }
+console.log(sum)
