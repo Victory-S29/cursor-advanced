38bb831ffb5703714426d8d2f9b89f6c66a9f343
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-03-30 22:27:06 +0300

    added homework 9

---
+++ hw09/script.js
@@ -0,0 +1,35 @@
+function generateBlocks(){ 
+    for(let i = 0; i < 5; i ++){
+        let row = document.createElement("div");
+        row.style.display = "flex";
+        document.body.appendChild(row);
+        for(let j = 0; j < 5; j ++){
+            let block = document.createElement("div");
+            block.setAttribute("id", `block${i}${j}`);
+            const rand = Math.round(Math.random() * (255 - 0));
+            const rand1 = Math.round(Math.random() * (255 - 0));
+            const rand2 = Math.round(Math.random() * (255 - 0));
+            block.style.width = "50px";
+            block.style.height = "50px";
+            block.style.backgroundColor = "red";
+            block.style.display = "block";
+            block.style.backgroundColor = `rgb(${rand}, ${rand1}, ${rand2})`;
+            row.append(block);
+        }
+    }
+}
+
+function generateBlocksInterval(){
+    for(let i = 0; i < 5; i++){
+        for(let j = 0; j < 5; j++){
+           let currentBlock = document.getElementById(`block${j}${i}`)
+           const color = Math.round(Math.random() * (255 - 0));
+           const color1 = Math.round(Math.random() * (255 - 0));
+           const color2 = Math.round(Math.random() * (255 - 0));
+           currentBlock.style.backgroundColor = `rgb(${color}, ${color1}, ${color2})`;
+        }
+    }
+}
+
+generateBlocks();
+setInterval(generateBlocksInterval, 1000);
