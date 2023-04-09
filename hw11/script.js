1835fe5be4b4052ddaa851a2b101a60f6ca66330
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-04-09 16:53:23 +0300

    added homework 11

---
+++ hw11/script.js
@@ -0,0 +1,13 @@
+async function getRandomChinese(lenght){
+    let i = 0;
+    let str = '';
+    while (i < lenght){
+        const sign = Date.now().toString().slice(-5);
+        str += String.fromCharCode(sign);
+        await new Promise(resolve => setTimeout(resolve, 50));
+        i++;
+    }
+    document.writeln(`<h1>${str}</h1>`);
+}
+
+getRandomChinese(4);
