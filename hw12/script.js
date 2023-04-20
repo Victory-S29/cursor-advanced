f24b495008829879c86a5ed72bd7d60b56fee4c3
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-04-20 16:58:37 +0300

    added hw 13

--- hw12/script.js
+++ hw12/script.js
@@ -1,24 +1,34 @@
-
 const film4 = "https://cdn.shopify.com/s/files/1/0043/8144/2141/products/IS_2a24ef2e-f8ca-4a2f-92bb-b0bf773e1f88.jpg?v=1645837528";
 const film5 = "https://s3.amazonaws.com/nightjarprod/content/uploads/sites/192/2022/04/21120853/k2J0GbxnuWJARxLHa2vAyO77qRX.jpg";
 const film6 = "https://upload.wikimedia.org/wikipedia/uk/thumb/b/b2/ReturnOfTheJediPoster1983.jpg/200px-ReturnOfTheJediPoster1983.jpg";
 const film1 = "https://upload.wikimedia.org/wikipedia/en/4/40/Star_Wars_Phantom_Menace_poster.jpg";
 const film2 = "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_.jpg";
 const film3 = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3d7e300c-7ac9-4543-92e8-59bdb6288a82/dfjc639-4a7b1896-5193-4940-8aa1-abc763d0e8b8.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzNkN2UzMDBjLTdhYzktNDU0My05MmU4LTU5YmRiNjI4OGE4MlwvZGZqYzYzOS00YTdiMTg5Ni01MTkzLTQ5NDAtOGFhMS1hYmM3NjNkMGU4YjguanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.60qeCB-jO8AnToso1omAOX51p3NqztogXBNGESsgXgo";
-
 const filmsImg = [film4, film5, film6, film1, film2, film3];
+
 const link = "https://swapi.dev/api/films/";
 const planetLink = "https://swapi.dev/api/planets/";
  
+let mainJSON = getJSON(link);
+let planetJSON = getJSON(planetLink);
+
+const filmInput = document.getElementById("film-input");
+const inputSubmit = document.getElementById("film-show");
+let numberOfFilm;
+
+getFilm(mainJSON);
+
+
+charactersPhoto = {
+    "1" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWOey035wnlxX7w0AzG8s0rQ70cwaT0k1JA&usqp=CAU"
+    ///........................
+}
+
 async function getJSON(link){
     let info = await fetch(link);
     let resJson = await info.json();
     return resJson;
 }
-
-let mainJSON = getJSON(link);
-let planetJSON = getJSON(planetLink);
-getFilm(mainJSON);
 
 function getFilm(resJson){
     document.getElementById("film-title").innerText = "Click film to see characters"
@@ -40,10 +50,30 @@ function filmRender(name, link, id){
     document.getElementById("content").innerHTML += filmStr;
 }
 
-function characterRender(name, birth, gender){
+function getCharacters(charactersPhoto, mainJSON, filmNumb){
+    mainJSON.then(film => {
+        film.results[filmNumb].characters.forEach(element => {
+            fetch(element).then(res => res.json()).then((character) => {
+                const str = character.url;
+                const idByNumber = (str.match(/\d+/g).join());
+                let link;
+                if (charactersPhoto[idByNumber]) {link = charactersPhoto[idByNumber]}
+                else {link = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRc8rYp_-q-PZbZquMwiXlanurk8R4Ov30Tw&usqp=CAU"}
+                characterRender(character.name, character.birth_year, character.gender, link);
+            }).catch(() =>{
+                let errStr = `<h2 class = "elem-h2">Couldn't load photo, try to reload the page</h2>`;
+                document.getElementById("content").innerHTML += errStr;
+            });
+        })
+        let mainBTN = `<button class= "btn" id = "characters-main">Main page</button>`;
+        document.body.innerHTML += mainBTN;
+    });
+}
+
+function characterRender(name, birth, gender, link){
     let characterStr = `
     <div class = "elem-div">
-    <img class = "elem-img" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRc8rYp_-q-PZbZquMwiXlanurk8R4Ov30Tw&usqp=CAU" alt="p">
+    <img class = "elem-img" src="${link}" alt="p">
     <p class = "elem-p">${name}</p>
     <p class = "elem-p">${birth}</p>
     <p class = "elem-p">${gender}</p>
@@ -52,8 +82,6 @@ function characterRender(name, birth, gender){
 }
 
 function getPlanets(planetJSON){
-    document.getElementById("film-title").innerText = ""
-    document.getElementById("content").innerHTML = "";
     planetJSON.then((planet) => {
         for (let i = 0; i < planet.results.length; i++){
             planetRender(planet.results[i].name, planet.results[i].terrain, i);
@@ -72,24 +100,6 @@ function planetRender(name, terrain, id){
     document.getElementById("content").innerHTML += planetStr;
 }
 
-function getCharacters(mainJSON, filmNumb){
-    mainJSON.then(film => {
-        film.results[filmNumb].characters.forEach(element => {
-            fetch(element).then(res => res.json()).then((character) => {
-                characterRender(character.name, character.birth_year, character.gender);
-            }).catch(() =>{
-                let errStr = `<h2 class = "elem-h2">Couldn't load photo, try to reload the page</h2>`;
-                document.getElementById("content").innerHTML += errStr;
-            });
-        })
-        let mainBTN = `<button class= "btn" id = "characters-main">Main page</button>`;
-        document.body.innerHTML += mainBTN;
-    });
-}
- 
-// film-show - to one film
-// planet-main - to planets 
-// characters-main - to main
 function showBtns(main, onefilm, character, planets){
     if (main){
         document.getElementById("characters-main").remove();
@@ -101,7 +111,6 @@ function showBtns(main, onefilm, character, planets){
         if(document.getElementById("planet-main")){document.getElementById("planet-main").remove();};
         document.getElementById("content").innerHTML = "";
         const elementsToRemove = document.querySelectorAll('#characters-main');
-        console.log("elementsToRemove" , elementsToRemove)
         if(elementsToRemove.length > 0){document.getElementById("characters-main").remove();};
     }
     if (character){
@@ -122,7 +131,7 @@ document.addEventListener('click', function(event) {
         if (pressDiv === `${i + 1}` || 
             (event.target.parentElement.id).toString()  === `${i + 1}`){
             showBtns(false, false, true, false)
-            getCharacters(mainJSON, i);
+            getCharacters(charactersPhoto, mainJSON, i);
         }
     }
 
@@ -139,7 +148,6 @@ document.addEventListener('click', function(event) {
     if ((event.target.id).toString() === "film-show"){
         event.preventDefault();
         let filmValue = numberOfFilm;
-        console.log(filmValue);
         if (filmValue && [1, 2, 3, 4, 5, 6].includes(Number(filmValue))) {
             showBtns(false, true, false, false);
             filmInfo(mainJSON, Number(filmValue) - 1);
@@ -150,26 +158,9 @@ document.addEventListener('click', function(event) {
     }
 })
 
-const filmInput = document.getElementById("film-input");
-const inputSubmit = document.getElementById("film-show");
-let numberOfFilm;
-
 document.addEventListener("input", e => {
     numberOfFilm = e.target.value;
 })
-
-// inputSubmit.addEventListener("click", e => {
-//     e.preventDefault();
-//     let filmValue = numberOfFilm;
-//     console.log(filmValue);
-//     if (filmValue && [1, 2, 3, 4, 5, 6].includes(Number(filmValue))) {
-//         showBtns(false, true, false, false);
-//         filmInfo(mainJSON, Number(filmValue) - 1);
-//     } else {
-//         alert("Incorrect value in form");
-//     }
-//     form.reset();
-// }) 
 
 function filmInfo(resJson, i){
     resJson.then((film) => {
@@ -192,5 +183,4 @@ function filmInfoRender(title, filmsImg, director, producer, opening_crawl){
     let mainBTN = `<button class= "btn" id = "characters-main">Main page</button>`;
     document.body.innerHTML += mainBTN;
 }
-
  
