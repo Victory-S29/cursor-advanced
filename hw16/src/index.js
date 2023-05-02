2c555a8b7a5e84d343f40ac03323c759647a5045
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-05-02 14:24:15 +0300

    test

--- hw16/src/index.js
+++ hw16/src/index.js
@@ -12,9 +12,5 @@ function App() {
   );
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
