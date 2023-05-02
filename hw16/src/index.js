c41a9bb0b7d13af49a71850484a8500418cddef0
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-05-02 15:42:50 +0300

    test

---
+++ hw16/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>HW16</h1>
+      </header>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
