d674e97f1924a4a7d81ce7406128958695ca6d57
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-05-10 00:09:52 +0300

    added advanced lvl for hw

--- hw18/src/Timer/Timer.js
+++ hw18/src/Timer/Timer.js
@@ -5,13 +5,15 @@ export default class Timer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time:  this.props.time,
-    }   
-    this.stopInterval = null;
+      time: this.props.time,
+    }
+    this.intervalFunction = null;
     this.isIntervalStop = true;
     this.onTick = this.props.onTick;
     this.autostart = this.props.autostart;
-    this.intervalNumber = this.props.intervalNumber
+    this.intervalNumber = this.props.intervalNumber;
+    this.onTimeEnd = this.props.onTimeEnd;
+    this.onTimeStart = this.props.onTimeStart;
   }
 
   componentDidMount() {
@@ -22,25 +24,35 @@ export default class Timer extends Component {
 
   stopTimer() {
     this.isIntervalStop = true;
-    clearInterval(this.stopInterval);
-    this.stopInterval = null;
+    clearInterval(this.intervalFunction);
   }
 
+  ontickRealisation() {
+    const initialTime = this.props.time;
+    if (initialTime === this.state.time) {
+      this.onTimeStart();
+    }
+    if (!this.isIntervalStop) {
+      this.onTick(this.formatTime(this.state.time));
+    }
+    if (this.state.time === 0) {
+      this.onTimeEnd();
+      this.setState({ time: initialTime + 1})
+    }
+  }
+ 
   startTimer() {
-    clearInterval(this.stopInterval);
+    clearInterval(this.intervalFunction);
     this.isIntervalStop = false;
-    this.stopInterval = setInterval(() => {
+    this.intervalFunction = setInterval(() => {
       this.setState(prevState => {
         if (prevState.time === 0) {
           this.stopTimer();
           return null;
         }
         return { time: prevState.time - 1 };
-      }, () => {
-        if (!this.isIntervalStop) {
-          this.onTick(this.formatTime(this.state.time));
-        }
-      });
+      }, this.ontickRealisation());
+      this.changeWidth();
     }, this.intervalNumber)
   }
 
@@ -50,21 +62,30 @@ export default class Timer extends Component {
     return `${minutes < 10 ? '0' : ''}${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   }
 
+  changeWidth() {
+    const initialTime = this.props.time;
+    const progress = (this.state.time / initialTime) * 100;
+    return progress
+  }
+
   render() {
-    console.log('time:', this.state.time);
     return (
-      <section> 
-        <h1 className='timer-h1'>Timer</h1>
-        <p className='time-show'>{this.formatTime(this.state.time)}</p>
-        <button className='btn-timer' onClick={() => {
-          this.autostart = false;
-          if (this.isIntervalStop) {
-            this.startTimer()
-          } else {
-            this.stopTimer()
-          }
-        }}> Start/Stop</button>
-      </section>
-      )
+        <section>
+          <div className='progressBar-container'>
+            <div className='progressBar' style={{ width: this.changeWidth() + '%'}}>
+            </div>
+          </div>
+          <h1 className='timer-h1'>Timer</h1>
+          <p className='time-show'>{this.formatTime(this.state.time)}</p>
+          <button className='btn-timer' onClick={() => {
+            this.autostart = false;
+            if (this.isIntervalStop) {
+              this.startTimer()
+            } else {
+              this.stopTimer()
+            }
+          }}> Start/Stop</button>
+        </section>
+    )
   }
 }
