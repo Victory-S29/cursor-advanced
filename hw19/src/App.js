78a55dad713dadeb15d82bb50d1da5909969ebc5
Вікторія Саніна <viktoriia.sanina.2917@gmail.com>
2023-05-12 21:25:45 +0300

    added profile page and fixed height

--- hw19/src/App.js
+++ hw19/src/App.js
@@ -1,9 +1,10 @@
-import NavBar from "./elements/NavBar"
+import NavBar from "./elements/NavBar";
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home"
-import Publication from "./pages/publication/Publication"
-import Photo from "./pages/photo/Photo"
+import Home from "./pages/home/Home";
+import Publication from "./pages/publication/Publication";
+import Photo from "./pages/photo/Photo";
+import Profile from "./pages/profile/Profile";
 
 const App = () => {
   return (<div>
@@ -11,6 +12,7 @@ const App = () => {
     <Routes>
       <Route path="photo" element={<Photo />} />
       <Route path="publication" element={<Publication />} />
+      <Route path="profile" element={<Profile />} />
       <Route index element={<Home />} />
     </Routes></div>
   );
